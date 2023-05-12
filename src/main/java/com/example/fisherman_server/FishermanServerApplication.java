a7d59e78d834b47aa28285db7ac828279ec24684
YuwenLu <luyuwen2000@163.com>
2023-05-12 14:59:05 -0700

    Project initialization

---
+++ src/main/java/com/example/fisherman_server/FishermanServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisherman_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishermanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishermanServerApplication.class, args);
+    }
+
+}
